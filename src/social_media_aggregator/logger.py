f4fed09dcbbba29ef468b43e6c2b9a509c964d63
jviegas6 <jviegas6@gmail.com>
2024-04-07 23:57:54 +0100

    bugfixing

--- src/social_media_aggregator/logger.py
+++ src/social_media_aggregator/logger.py
@@ -9,7 +9,6 @@ class CustomLogger:
     """
     Class with methods to create a custom logger
     and contains the following methods:
-    
     - _validate_logger: validates the logger name and level
     - _create_formatter: creates a custom formatter for the logger
     - _create_handler: creates a custom handler for the logger
